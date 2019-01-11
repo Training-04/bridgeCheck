dc854206995d9153e1e5c80dc4e938424e763a00
ran wu <wuranvv@outlook.com>
2019-01-11 11:20:27 +0800

    modify pom

--- web/src/main/java/group/bridge/web/WebApplication.java
+++ web/src/main/java/group/bridge/web/WebApplication.java
@@ -1,10 +1,11 @@
 package group.bridge.web;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
---
+++ web/src/main/java/group/bridge/web/configration/ThymeleafConfig.java
@@ -0,0 +1,13 @@
+package group.bridge.web.configration;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+
+public class ThymeleafConfig {
+
+}
