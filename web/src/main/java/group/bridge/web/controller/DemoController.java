4f9a7117b1dfeaa3c9c468ecb59e6349beb67902
ran wu <wuranvv@outlook.com>
2019-01-11 10:10:37 +0800

    add demo

--- web/src/main/java/group/bridge/web/controller/DemoController.java
+++ web/src/main/java/group/bridge/web/controller/DemoController.java
@@ -1,21 +1,17 @@
 package group.bridge.web.controller;
 
-import group.bridge.web.entity.Car;
-import group.bridge.web.entity.Person;
-import group.bridge.web.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.persistence.criteria.*;
-import javax.persistence.metamodel.EntityType;
-import javax.persistence.metamodel.SingularAttribute;
-import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class DemoController {
-
+    @RequestMapping("/")
+    public String demo(Model model) {
+        model.addAttribute("title","test");
+        return "demo/fragment1";
+    }
 
 }
