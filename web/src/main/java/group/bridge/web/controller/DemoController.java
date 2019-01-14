513ec6f7b4337a71f2598d07735bd0e9bb928b13
ran wu <wuranvv@outlook.com>
2019-01-14 10:51:30 +0800

    add pageable

--- web/src/main/java/group/bridge/web/controller/DemoController.java
+++ web/src/main/java/group/bridge/web/controller/DemoController.java
@@ -1,6 +1,8 @@
 package group.bridge.web.controller;
 
 
+import group.bridge.web.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
+    @Autowired
+    PersonService personService;
     @RequestMapping("/")
     public String demo(Model model) {
         model.addAttribute("title","test");
--- web/src/main/java/group/bridge/web/service/BaseService.java
+++ web/src/main/java/group/bridge/web/service/BaseService.java
@@ -1,15 +1,27 @@
 package group.bridge.web.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
 public interface BaseService<T,ID> {
     boolean add(T t);
+
     boolean update(T t);
+
     void delete(T t);
+
     void deleteById(ID id);
+
     List<T> getAll();
+
+    Page<T> getAll(Pageable pageable);
+
     T get(ID id);
+
     List<T> getByPredicate(Specification<T> specification);
+
+    Page<T> getByPredicate(Specification<T> specification, Pageable pageable);
 }
