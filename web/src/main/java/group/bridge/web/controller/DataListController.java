a621d90031b4d21daf26fcf3b4dd09237ecb4400
Ziyooo <253521700@qq.com>
2019-01-17 10:15:25 +0800

    四个页面的跳转

--- web/src/main/java/group/bridge/web/controller/DataListController.java
+++ web/src/main/java/group/bridge/web/controller/DataListController.java
@@ -4,20 +4,37 @@ package group.bridge.web.controller;
 import group.bridge.web.service.BridgeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class DataListController {
     @Autowired
     private BridgeService bridgeService;
 
-    @RequestMapping("/datalist")
-    public String Datalist()
+    @RequestMapping("/real_time_datalist")
+    public String Real_time_datalist()
     {
-        return "demo/test";
+        return "/demo/real_time_datalist";
     }
+
+
+    @RequestMapping("/history_datalist")
+    public String History_datalist()
+    {
+        return "/demo/history_datalist";
+    }
+
+    @RequestMapping("/histroy_warn_record")
+    public String Histroy_warn_record()
+    {
+        return "/demo/histroy_warn_record";
+    }
+
+    @RequestMapping("/statistic_datalist")
+    public String Statistic_datalist()
+    {
+        return "/demo/statistic_datalist";
+    }
+
+
 }
