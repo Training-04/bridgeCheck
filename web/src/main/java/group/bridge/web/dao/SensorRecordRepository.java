3ec284a59a4141700b732c6c1115778861894a9a
Ziyooo <253521700@qq.com>
2019-02-22 17:29:45 +0800

    改__ztt

--- web/src/main/java/group/bridge/web/dao/SensorRecordRepository.java
+++ web/src/main/java/group/bridge/web/dao/SensorRecordRepository.java
@@ -16,10 +16,12 @@ public interface SensorRecordRepository extends BaseRepository<SensorRecord, Int
 
     List<SensorRecord> findAllByOrderByDateDesc();
     Page<SensorRecord> findAllByOrderByDateDesc(Pageable pageable);
+    // 按桥梁名称查询，按日期排序
     @Query(nativeQuery = true,
             value = "SELECT record_id, date, value, sr.sensor_id FROM  sensor_records sr, sensors s WHERE sr.sensor_id = s.sensor_id AND s.bridge_id = :bridge_id ORDER BY date DESC;")
     List<SensorRecord> findAllByBridgeByDateDesc(@Param("bridge_id") Integer bridge_id);
 
+    // 按桥梁名、传感器名查询，按日期排序
     @Query(nativeQuery = true,
             value = "SELECT record_id, date, value, sr.sensor_id FROM  sensor_records sr, sensors s WHERE sr.sensor_id = s.sensor_id AND s.bridge_id = :bridge_id  AND s.para_unit_cn = :para_unit_cn ORDER BY date DESC;")
     List<SensorRecord> findAddByBridgeBySensorByDateDesc(@Param("bridge_id") Integer bridge_id, @Param("para_unit_cn") String para_unit_cn);
